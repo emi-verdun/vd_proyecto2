8ef00f65d99b737eea8adf8a15351afaf4322691
Emi Verdun <emiverdun@outlook.es>
2024-04-16 18:48:47 -0300

    add café + servicio

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,17 @@
   import * as d3 from "d3"
   import {onMount} from "svelte"
 
+  
   /* Array donde guardaremos la data */
   let datos_morfi = []
 
+
   /* 1. Escala par lugar de comida */
   let colorLugar = d3.scaleOrdinal()
-    .domain(["Mingo", "Fud", "Green Bites", "El Rincón", "Havana", "Traigo de casa"])
+    .domain(["Mingo", "Fud", "Green Bites", "El Rincon", "Havana", "Traigo de casa"])
     .range(["#ff8c8c", "#ff814a", "#458330", "#71372a", "#ffc700", "#ff2f48"])
 
-  /* 2. Escala para mejor café */
+  /* 2. Escala para mejor café */ /*NO SE USA, NO DEJA USARLO CON SVG EN ESTE ARCHIVO, BORRA LA PAGINA*/
   /*let colorCafe = d3.scaleOrdinal
     .domain(["Havana", "Fud"])
     .range(["#ffb700", "#71372a"])
@@ -23,7 +25,20 @@
     .domain([0,1,2,3,4,5])
     .range([1,1,2,3,4,5])
 
-  /* Creo que nada más necesita escala */
+  /* 5. Escala para rating de servicio */
+  let servicio = d3.scaleOrdinal()
+    .domain([0,1,2,3,4,5])
+    .range([1,1,2,3,4,5])
+  
+  /* 6. Escala para rating del tostado de Mingo */
+  let tostadoM1 = d3.scaleBand()
+    .domain([0,1,2,3,4,5])
+    .range([30, 80])
+  
+  let tostadoM2 = d3.scaleBand()
+  .domain([0,1,2,3,4,5])
+  .range([10, 60])
+
 
   onMount(() => {
     d3.csv("./data/MorfiFacu-sheet.csv", d3.autoType).then(data => {
@@ -57,21 +72,10 @@
       <div class="comida-container">
 
         <div class="sanguches">
-          
           {#each d3.range(1, 6) as n}
-            {#if n <= [cantDias(sangu.cantDias)]} <!-- No me deja usar {cantDias(sangu.cantDias)}-->
-              <div class="pan"
-                style="background-color: #fffbd6;
-                width: 50px;
-                height: 16px;
-                display: flex;
-                justify-content:center;
-                align-items: center;
-                border-style: solid;
-                border-width: 1px;
-                border-color: #ffe6b6"
-              >
-              
+            {#if n <= [cantDias(sangu.cantDias)]}
+
+              <div class="pan">
                 <div class="relleno">
                   <div class="fill_1"
                     style="background-color: {colorLugar(sangu.lugar)};
@@ -84,11 +88,10 @@
                     width: 50px;" 
                   ></div>
                 </div>
-                
               </div>
+
             {/if} 
           {/each}
-
         </div>
 
         <div class="plato"
@@ -107,19 +110,36 @@
           ></div> 
         </div>
 
-        <!--<div class="cafe">
+        <div class="cafe">
+          {#if sangu.mejorCafe == 'Havana'}
+            <img style= "height: 45px" src="/images/cafeHavanna.svg" alt="Café de Havanna">
+          {:else}
+            <img style= "height: 45px" src="/images/cafeFud.svg" alt="Café de Fud">
+          {/if}
         </div>
 
         <div class="servicio">
-        </div>-->
+          {#if 1 == [servicio(sangu.ratingServicio)]}
+            <img style= "height: 30px" src="/images/servicio1.svg" alt="Rating de Servicio = 0-1">
+          {:else if 2 == [servicio(sangu.ratingServicio)]}
+            <img style= "height: 12px" src="/images/servicio2.svg" alt="Rating de Servicio = 2">
+          {:else if 4 == [servicio(sangu.ratingServicio)]}
+            <img style= "height: 25px" src="/images/servicio4.svg" alt="Rating de Servicio = 4">
+          {:else if 5 == [servicio(sangu.ratingServicio)]}
+            <img style= "height: 35px" src="/images/servicio5.svg" alt="Rating de Servicio = 5">
+          {/if}
+        </div>
 
       </div>
+
     {/each}
+
   </div> 
 </main>
 
 
 <style>
+
   .header {
     display: flex;
     justify-content: center;
@@ -129,6 +149,7 @@
     margin-bottom: 80px;
     background-color: whitesmoke;
   }
+
   .headline {
     font-size: 40px;
     font-family: Passion One;
@@ -139,6 +160,7 @@
     text-align: center;
     margin: 20px;
   }
+
   .bajada {
     font-size: 18px;
     font-family: sans-serif;
@@ -147,6 +169,7 @@
     margin: 5px;
     margin-bottom: 20px;
   }
+
   .container {
     display: flex;
     justify-content: center;
@@ -156,6 +179,7 @@
     gap: 40px;
     margin-bottom: 100px;
   }
+
   .comida-container {
     display: flex;
     justify-content: center;
@@ -163,26 +187,16 @@
     align-items: center;
     flex: 180px 0 0;
   }
-  /*.person {
-    width: 100px;
-    height: 100px;
-    border: 10px solid black;
-    border-radius: 50%;
-    box-sizing: border-box;
-    background-color: pink;
+  .pan {
+    background-color: #fffbd6;
+    width: 50px;
+    height: 16px;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #ffe6b6
   }
-  .medal {
-    width: 15px;
-    height: 15px;
-    border-radius: 50%;
-    background-color: gold;
-    margin: 5px 0;
-  }
-  .name {
-    font-size: 14px;
-    color: rgb(65, 65, 65);
-    font-weight: normal;
-    text-align: center;
-    margin-top: 5px;
-  }*/
+
 </style>
